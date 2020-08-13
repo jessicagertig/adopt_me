2439d85a7e86f8ccdaaebb66d6624055d1b422cf
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-13 15:51:36 -0500

    Add react as dependency. Separate into components.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
 
 //your code is going to go here
 const App = () => {
@@ -28,4 +24,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
