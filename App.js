c080fd1a16698ca405849141c0bdc720c62691e8
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-12 19:36:46 -0500

    First project commit.  Commit point as decided by class instructor.

---
+++ App.js
@@ -0,0 +1,31 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, name),
+    React.createElement('h2', {}, animal),
+    React.createElement('h2', {}, breed),
+  ]);
+};
+
+//your code is going to go here
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Teddy',
+      animal: 'Dog',
+      breed: 'German Shepherd',
+    }),
+    React.createElement(Pet, {
+      name: 'George',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, {
+      name: 'Vashti',
+      animal: 'Cat',
+      breed: 'Calico',
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
