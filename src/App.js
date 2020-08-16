56c73c58739a43804293e5261e0c803089050b5a
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-15 21:34:35 -0500

    Converting to JSX and configuring Tools.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
-//your code is going to go here
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Teddy",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-    React.createElement(Pet, {
-      name: "George",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Vashti",
-      animal: "Cat",
-      breed: "Calico",
-    }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Teddy",
+  //     animal: "Dog",
+  //     breed: "German Shepherd",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "George",
+  //     animal: "Bird",
+  //     breed: "Cockatiel",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Vashti",
+  //     animal: "Cat",
+  //     breed: "Calico",
+  //   }),
+  // ]);
+  return (
+    <div>
+      <h1 id='something important'>Adopt Me!</h1>
+      <Pet name="Teddy" animal="Dog" breed="German Shepherd" />
+      <Pet name="George" animal="Bird" breed="Cockatiel" />
+      <Pet name="Vashti" animal="Cat" breed="Calico" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
