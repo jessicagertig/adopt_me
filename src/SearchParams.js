3deaf131861104494ff233255922fff76c067882
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-15 22:02:31 -0500

    Add change handler to location state.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  //initial state, state management
+  const [location, setLocation] = useState("Seattle, WA");
 
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           location
-          <input id="location" value={location} placeholder="Location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
         </label>
         <button>Submit</button>
       </form>
