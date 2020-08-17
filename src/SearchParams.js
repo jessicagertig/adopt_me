0f449a55334e6e8bfb69ef0d732aca49beeb6af4
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-17 01:00:19 -0500

    Used dropDown hook in searchParams component.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
   //initial state, state management
   const [location, setLocation] = useState("Seattle, WA");
-  const [animal, setAnimal] = useState("dog");
-  const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", 'dog', ANIMALS)
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds)
 
   return (
     <div className="search-params">
@@ -20,35 +21,8 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
-            onBlur={(e) => setAnimal(e.target.value)}
-          >
-            <option>all</option>
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>{animal}</option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="breed">
-          Animal
-          <select
-            id="breed"
-            value={breed}
-            onChange={(event) => setBreed(event.target.value)}
-            onBlur={(e) => setBreed(e.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option>all</option>
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>{breedString}</option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
