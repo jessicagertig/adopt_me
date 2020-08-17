f3850ccbbe6a555f945296e4bb9bf2d84287281c
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-17 00:35:27 -0500

    Added ANIMALS api package. Added select to form.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   //initial state, state management
   const [location, setLocation] = useState("Seattle, WA");
-  // const [animal, setAnimal] = useState("Dog")
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -16,6 +17,20 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(event) => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(event) => setAnimal(event.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>all</option>
+            {ANIMALS.map((animal) => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
