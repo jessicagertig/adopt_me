d99142d5b96d99586cc897682601e0c806929c58
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-19 19:39:45 -0500

    Creating details component as a Class componenent.

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,33 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    //initial state
+    this.state = {
+      loading: true,
+    };
+  }
+
+  //only occurs when page first renders, good for api calls
+  componentDidMount() {
+    //pet is an api call
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    });
+  }
+
+  render() {}
+}
 
 export default Details;
