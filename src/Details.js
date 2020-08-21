fd222eee73348352449c922661431b686d9ea52f
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-20 21:32:08 -0500

    Added Carousel component, babel config.

--- src/Details.js
+++ src/Details.js
@@ -1,15 +1,17 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import Carousel from './Carousel';
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-
-    //initial state
-    this.state = {
-      loading: true,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   //initial state
+  //   this.state = {
+  //     loading: true,
+  //   };
+  // }
+  //replaced with below code after setting up babel plugins, eslint config, added parser to selintrc file
+  state = { loading: true };
 
   //only occurs when page first renders, good for api calls
   componentDidMount() {
@@ -32,10 +34,11 @@ class Details extends React.Component {
       return <h1>loading...</h1>
     } 
 
-    const { animal, breed, location, description, name } = this.state
+    const { animal, breed, location, description, name, media } = this.state
 
     return (
       <div className="details">
+        <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
