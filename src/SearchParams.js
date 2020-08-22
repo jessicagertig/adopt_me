af8f8ba7727d2a7e8c49a642c350a7ba838ed40d
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-22 14:55:27 -0500

    Added global theme from Context to SearchParams component.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import Results from "./Results";
 import useDropdown from "./useDropdown";
+import ThemeContext from './ThemeContext';
 
 const SearchParams = () => {
   //initial state, state management
@@ -11,6 +12,7 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
+  const [theme] = useContext(ThemeContext)
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -52,7 +54,7 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
-        <button>Submit</button>
+        <button style={{backgroundColor: theme}}>Submit</button>
       </form>
       <Results pets={pets} />
     </div>
