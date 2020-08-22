05d2c096acad68312ff16cae69b2165ff4afcb13
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-21 21:01:11 -0500

    Created ErrorBoundary component per react docs for error handling.

---
+++ src/ErrorBoundary.js
@@ -0,0 +1,28 @@
+//mostly code from reactjs.org/docs/error-boundaries.html
+
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+  componentDidCatch(error, info) {
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>
+          There was an error with this listing. <Link to="/">Click here</Link>{" "}
+          to go back to the homepage or wait five seconds.
+        </h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
