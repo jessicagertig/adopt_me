4ecd5c9e13d091882a37bbd4074da23e354613d7
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-22 15:17:08 -0500

    Adding ability to change state of theme.  Added setTheme in SearchParams. Made dropdown with options.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -12,7 +12,7 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
   const [pets, setPets] = useState([]);
-  const [theme] = useContext(ThemeContext)
+  const [theme, setTheme] = useContext(ThemeContext)
 
   async function requestPets() {
     const { animals } = await pet.animals({
@@ -54,6 +54,20 @@ const SearchParams = () => {
         </label>
         <AnimalDropdown />
         <BreedDropdown />
+        <label htmlFor="theme">
+          Theme
+          <select 
+            value={theme}
+            onChange={e => setTheme(e.target.value)}
+            onBlur={e => setTheme(e.target.value)}
+          >
+            <option value="peru">Peru</option>
+            <option value="darkblue">Dark Blue</option>
+            <option value="mediumorchid">Medium Orchid</option>
+            <option value="chartreuese">Chartreuese</option>
+            <option value="pink">Pink</option>
+          </select>
+        </label>
         <button style={{backgroundColor: theme}}>Submit</button>
       </form>
       <Results pets={pets} />
